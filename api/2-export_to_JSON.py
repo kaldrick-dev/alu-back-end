ddf4b96afeddf0a1dab9bfd8044897542bb0c9de
kaldrick-dev <a.mpanokal@alustudent.com>
2026-02-26 14:59:10 +0200

    Fixed formatting

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -8,23 +8,36 @@ import sys
 
 def main():
     """Main function."""
+    if len(sys.argv) != 2:
+        return
+
     user_id = int(sys.argv[1])
-    todo_url = "https://jsonplaceholder.typicode.com/todos"
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+    todo_url = (
+        "https://jsonplaceholder.typicode.com/todos?userId={}".format(user_id)
+    )
+    user_url = (
+        "https://jsonplaceholder.typicode.com/users?id={}".format(user_id)
+    )
 
     response = requests.get(todo_url)
-    user_name = requests.get(user_url).json().get("username")
+    user_response = requests.get(user_url)
+    users = user_response.json()
+    if not users:
+        return
+
+    user_name = users[0].get("username")
     user_data = []
-    output = {user_id: user_data}
+    output = {str(user_id): user_data}
 
     for todo in response.json():
-        if todo.get("userId") == user_id:
-            user_data.append(
-                {
-                    "task": todo.get("title"),
-                    "completed": todo.get("completed"),
-                    "username": user_name,
-                })
+        user_data.append(
+            {
+                "task": todo.get("title"),
+                "completed": todo.get("completed"),
+                "username": user_name,
+            }
+        )
+
     file_name = "{}.json".format(user_id)
     with open(file_name, "w") as file:
         json.dump(output, file)
