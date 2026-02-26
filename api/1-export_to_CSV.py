181298699b0b1be7a2c5c3d082358dcf9da9da7f
kaldrick-dev <a.mpanokal@alustudent.com>
2026-02-26 14:57:28 +0200

    Fixed formatting

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -8,29 +8,40 @@ import sys
 
 def main():
     """Main function."""
-    user_id = int(sys.argv[1])
-    todo_url = "https://jsonplaceholder.typicode.com/todos"
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+    if len(sys.argv) != 2:
+        return
 
-    file_content = []
+    user_id = int(sys.argv[1])
+    todo_url = (
+        "https://jsonplaceholder.typicode.com/todos?userId={}".format(user_id)
+    )
+    user_url = (
+        "https://jsonplaceholder.typicode.com/users?id={}".format(user_id)
+    )
 
     response = requests.get(todo_url)
-    user_name = requests.get(user_url).json().get("username")
+    user_response = requests.get(user_url)
+    users = user_response.json()
+    if not users:
+        return
 
+    user_name = users[0].get("username")
+
+    file_content = []
     for todo in response.json():
-        if todo.get("userId") == user_id:
-            file_content.append(
-                [str(user_id),
-                 user_name,
-                 todo.get("completed"),
-                 "{}".format(todo.get("title"))])
+        file_content.append(
+            [
+                str(user_id),
+                user_name,
+                todo.get("completed"),
+                todo.get("title"),
+            ]
+        )
 
     file_name = "{}.csv".format(user_id)
     with open(file_name, "w", newline="") as csv_file:
         csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
         for row in file_content:
-            for item in row:
-                str(item)
             csv_writer.writerow(row)
 
 
